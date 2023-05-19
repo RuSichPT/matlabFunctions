d1d6cc3469b224a4a9ccf909fade9c1e899ea90f
pavel.tokarev <tokarevpavel1996@mail.ru>
2023-05-19 11:29:06 +0300

    Переделал mimoCapacity.m

--- Capacity/mimoCapacity.m
+++ Capacity/mimoCapacity.m
@@ -1,4 +1,4 @@
-function C = mimoCapacity(H, snr_dB)
+function [C, lambda] = mimoCapacity(H, snr_dB)
     % H - матрица канала размерностью [Nrx Ntx]
     % F - матрица прекодирования
     % snr_dB - в дБ
@@ -7,7 +7,11 @@ function C = mimoCapacity(H, snr_dB)
     numTx = size(H,2);
     
     C = zeros(1,length(snr));
-    for i = 1:length(snr_dB)
-        C(i) = 1/numRx * log2(det(eye(numRx) + 1/numTx*snr(i)*(H*H'))); 
+    for i = 1:length(snr_dB) 
+        sigma = svd(H); % eig(H*H') = svd(H)^2 
+        lambda = sigma.^2;
+        C(i) = 1/numRx * sum(log2(1 + 1/numTx*snr(i)*abs(lambda))); % 1/numSTS нормировка по потокам 
+%         C(i) = 1/numRx * log2(det(eye(numRx) + 1/numTx*snr(i)*(H*H')));  для проверки
     end
+    
 end
